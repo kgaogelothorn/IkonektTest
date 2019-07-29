51b9d7e31c76d2825309b1962643af133f3b7d88
Kgaogelothorn <kgaogelothorn@gmail.com>
2019-07-29 13:03:50 +0200

    feat(init):initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'IkonektTest';
+
+  cartNumber = 55;
+
+  nav = [
+    { name: 'Home', link: '', },
+    {name: 'Shop Brands', link: '', },
+    {name: 'Shop Product Type', link: '', }
+  ];
 }

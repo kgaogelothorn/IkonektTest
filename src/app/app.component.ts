d5de926f5c15368c2e0ec5c3c3e90356a06536e6
Kgaogelothorn <kgaogelothorn@gmail.com>
2019-07-29 22:43:01 +0200

    refactor(app):modified component app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'IkonektTest';
 
-  cartNumber = 55;
+  cartNumber = 0;
 
   nav = [
-    { name: 'Home', link: '', },
+    {name: 'Home', link: '', },
     {name: 'Shop Brands', link: '', },
     {name: 'Shop Product Type', link: '', }
+  ];
+
+  products = [
+    {price: 'R100.00 - R 5000.00', name: 'Skin Renewal Gift Voucher',
+    image: 'Skin-Renewal-Gift-Voucher-300x300', headline: 'Gift Vouchers', wishlistIcon: 'fa fa-heart', btn: 'Add to Wishlist', },
+    {price: 'R130', image: 'Lamelle-Clarity-Active-Control-300x300', headline: './../assets/lamelle.jpg',
+    wishlistIcon: 'fa fa-heart', btn: 'Add to Wishlist', },
+    {price: 'R175', name: 'Alpha Lipoic Acid', image: 'Alpha-Lipoic-Acid-300x300', headline: 'Oral Supplement',
+     wishlistIcon: 'fa fa-heart', btn: 'Add to Wishlist', },
+    {price: 'R175', name: 'Apple Cider Vinega', image: 'Apple-Cider-Vinegar-300x300', headline: 'Oral Supplement',
+     wishlistIcon: 'fa fa-heart', btn: 'Add to Wishlist', },
   ];
 }
